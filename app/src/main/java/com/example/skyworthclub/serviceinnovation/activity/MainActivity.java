5aa76798047bf561653f9f1b9ce7f3d81c668095
SkyworthClub <skyworthclub@SkyworthClubdeMacBook-Pro.local>
2018-01-21 17:39:59 +0800

    首页初步布局

---
+++ app/src/main/java/com/example/skyworthclub/serviceinnovation/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.skyworthclub.serviceinnovation.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.skyworthclub.serviceinnovation.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.xyj_main);
+    }
+}
