cabe3cb520ab1cb43e55ab76afeb0e5f5f5293e9
Saujyun <2679225466lhy@gmail.com>
2018-03-25 23:21:47 +0800

    mmine的布局更新

--- app/src/main/java/com/example/skyworthclub/serviceinnovation/Mine/fragment/Mine.java
+++ app/src/main/java/com/example/skyworthclub/serviceinnovation/Mine/fragment/Mine.java
@@ -7,9 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.skyworthclub.serviceinnovation.Mine.activity.Mine_add_sub;
+import com.example.skyworthclub.serviceinnovation.Mine.activity.Mine_data;
 import com.example.skyworthclub.serviceinnovation.Mine.activity.Mine_resume;
 import com.example.skyworthclub.serviceinnovation.R;
 
@@ -17,21 +20,68 @@ import com.example.skyworthclub.serviceinnovation.R;
  * Created by skyworthclub on 2018/1/21.
  */
 
-public class Mine extends Fragment {
-    TextView textView;
+public class Mine extends Fragment implements View.OnClickListener {
+    ImageView data;
+    ImageView subscribe;
+    ImageView resume;
+    ImageView info;
+    ImageView setting;
+    ImageView avatar;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable final Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.mine_main, container, false);
-        textView=view.findViewById(R.id.mine_text);
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(v.getContext(),"click",Toast.LENGTH_SHORT).show();
-                Intent intent=new Intent(v.getContext(), Mine_resume.class);
-                startActivity(intent);
-            }
-        });
+        initView(view);
         return view;
+    }
+
+    private void initView(View view) {
+        setting=view.findViewById(R.id.mine_setting);
+        data=view.findViewById(R.id.mine_data_button);
+        subscribe=view.findViewById(R.id.mine_sub_button);
+        resume=view.findViewById(R.id.mine_resume_button);
+        info=view.findViewById(R.id.mine_info_button);
+        avatar=view.findViewById(R.id.mine_avatar);
+        setting.setOnClickListener(this);
+        data.setOnClickListener(this);
+        subscribe.setOnClickListener(this);
+        resume.setOnClickListener(this);
+        info.setOnClickListener(this);
+        avatar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.mine_setting:{
+                Toast.makeText(v.getContext(), "setting", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.mine_data_button:{
+                Toast.makeText(v.getContext(), "data", Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(v.getContext(),Mine_data.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.mine_resume_button:{
+                Toast.makeText(v.getContext(), "resume", Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(v.getContext(),Mine_resume.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.mine_sub_button:{
+                Toast.makeText(v.getContext(), "subscribe", Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(v.getContext(),Mine_add_sub.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.mine_info_button:{
+                Toast.makeText(v.getContext(), "info", Toast.LENGTH_SHORT).show();
+//                Intent intent=new Intent(v.getContext(),Mine_data.class);
+//                startActivity(intent);
+                break;
+            }
+        }
+
     }
 }
