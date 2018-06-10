5b5bc79f1afbbef17a65f74279427c20284ab9de
HT <374984629@qq.com>
2018-06-10 12:56:59 +0800

    file download funtion

---
+++ app/src/main/java/com/example/skyworthclub/serviceinnovation/Mine/utils/Constant.java
@@ -0,0 +1,24 @@
+package com.example.skyworthclub.serviceinnovation.Mine.utils;
+
+/**
+ * Created by dn on 2018/6/10.
+ */
+
+public class Constant {
+    public static String ORIGINAL_URL = "http://127.0.0.1/";
+    //登录
+    public static String LOGIN_URL = "http://127.0.0.1/user/login/";
+    //注册
+    public static String REGISTER_URL = "http://127.0.0.1/user/register/";
+    //获取验证码
+    public static String GET_CODE_URL = "http://127.0.0.1/identity/";
+    //修改密码
+    public static String RESET_PASSWORD_URL = "http://127.0.0.1/user/forget/";
+    //上传头像
+    public static String UPLOAD_PHOTO_URL = "http://127.0.0.1/user/upload/";
+    //验证验证码
+    public static String CONFORM_CODE_URL = "http://127.0.0.1/user/idCodeConfirm/";
+    //下载文件
+    public static String DOWNLOAD_FILE_URL = "http://127.0.0.1/project/requestFile/download/";
+
+}
